9ebd0f40760cd5771e646843cbd11a4cf824950a
venkateshmainalli <43678017+venkateshmainalli@users.noreply.github.com>
2020-06-26 20:43:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+Initial Security()
+{
+ //Sample Code
+}
+
+feature1()
+{
+  //Sample Code for Feature 1
+}
+
+Initial Security()
+{
+  //sample code
+}
+
+}
+
